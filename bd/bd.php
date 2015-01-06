921c9e5a95fe652248ef7bcbbd6f9f1b3fd87384
1111341 <ibrahimabahdlm@hotmail.com>
2015-01-06 14:42:16 -0500

    Connexion a la BD

---
+++ bd/bd.php
@@ -0,0 +1,17 @@
+<?php
+	// Paramètres de connexion à la BD.
+	require("include/params.inc");
+	
+	// Création d'une connexion à la BD.
+	try 
+	{
+		$connBD = new PDO("mysql:host=$dbHote; dbname=$dbNom", $dbUtilisateur, $dbMotPasse, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		
+		// Pour lancer les exceptions lorsqu'il y des erreurs PDO.
+		$connBD -> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $e) 
+	{
+		exit( "Erreur lors de la connexion à la BD :<br />\n" .  $e->getMessage() );
+	}
+?>
