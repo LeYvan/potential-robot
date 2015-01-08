fb580e4e9e9315b58a1cb83c5e114bdb187ef25f
Charles-André Beaudry <ca_beaudry@yahoo.ca>
2015-01-07 22:18:46 -0500

    Ai des require_once sur tout les fichiers sur lesquels j'ai travaillé

--- sinistres/index.php
+++ sinistres/index.php
@@ -80,10 +80,6 @@ require_once($base."include/header.php");
 		ctx.fill();
     }
     </script>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php
+require_once($base."include/footer.php");
+?>
